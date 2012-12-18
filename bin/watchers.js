d2785d5bf5a853f29b05a93c96a4c3b0efc67be2
unknown <alejandroe@gmail.com>
2012-12-18 11:05:48 -0300

    debounce

--- bin/watchers.js
+++ bin/watchers.js
@@ -2,6 +2,7 @@
 var path    = require('path');
 var fs      = require('fs');
 var io      = require('faye');
+var _       = require('underscore');
 var portscanner = require('portscanner');
 var argv    = require('optimist').argv;
 var lib     = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
--- src_client/chrome_extenson/src/dev_stylus/stylus_compile_pry.bat
+++ src_client/chrome_extenson/src/dev_stylus/stylus_compile_pry.bat
@@ -1,2 +1,2 @@
-Start k:\portable\ConEmu\ConEmu.exe /cmd  watchers d:\xampplite\htdocs\watchers\src_client\chrome_extenson\src\ -o .css -n WatchStyleCss
+Start k:\portable\ConEmu\ConEmu.exe /cmd node d:\xampplite\htdocs\watchers\lib\main.js d:\xampplite\htdocs\watchers\src_client\chrome_extenson\src\ -o .css -n WatchStyleCss
 Start k:\portable\ConEmu\ConEmu.exe /cmd  stylus -w -u nib -c -o ../
